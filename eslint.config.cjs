d04381049d937e41ee4f1e2e93ec00b52cf7f9b2
Brian <brian.elmeskiri@icloud.com>
2024-06-11 10:08:04 +0200

    Initial commit

---
+++ eslint.config.cjs
@@ -0,0 +1,19 @@
+const globals = require('globals');
+
+module.exports = [
+    {
+        files: ['**/*.js'],
+        languageOptions: {
+            ecmaVersion: 2021,
+            sourceType: 'commonjs',
+            globals: {
+                ...globals.node
+            }
+        },
+        rules: {
+            'semi': ['error', 'always'],
+            'quotes': ['error', 'single'],
+            // Ajoutez vos règles ici
+        }
+    }
+];
