d9f879cdc5a54785bff653b871e9d2349c72361c
Brian <brian.elmeskiri@icloud.com>
2024-06-12 13:56:43 +0200

    ✨ Feature : Use eslint

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,19 +1,19 @@
 class FizzBuzz {
     static getOutput(number) {
         if (number >= 1000)
-            throw new Error('Number should not be greater than 1000')
+            throw new Error('Number should not be greater than 1000');
 
         if (number % 3 === 0 && number % 5 === 0)
-            return "FizzBuzz"
+            return 'FizzBuzz';
 
         if (number % 3 === 0)
-            return "Fizz"
+            return 'Fizz';
 
         if (number % 5 === 0)
-            return "Buzz"
+            return 'Buzz';
 
-        return number.toString()
+        return number.toString();
     }
 }
 
-module.exports = FizzBuzz
+module.exports = FizzBuzz;
